cf1e252fbd7ba9e0e08c95724e655f54178f4baf
Prafull Bhosale <66156282+prafull-bhosale@users.noreply.github.com>
2022-08-16 02:55:40 +0530

    1D_Elasto_Viscoplasticity code files upload

---
+++ 1D_Elasto_Viscoplasticity_a3/t=t1/t1=150.0_c=0.0002/delta_t=2.5/job_run.bat
@@ -0,0 +1,2 @@
+del D:\MTechDesign\Sem4\ME759_NonlinearFEM\Codes\Assignment3\fortran_code_output.csv
+abaqus job=ME759_assign3_delta_t=2.5 user=D:\MTechDesign\Sem4\ME759_NonlinearFEM\Codes\Assignment3\Elasto-Viscoplasticity_1D.for
